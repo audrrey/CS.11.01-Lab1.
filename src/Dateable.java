756606f9aedb19baa3adc65c09db0dec88e86e1e
techarenz <57818506+techarenz@users.noreply.github.com>
2023-08-31 11:53:19 +0800

    Update README.md

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
-    }
-}
+
+        class AgeDatingRule {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter your age: ");
+                int age = scanner.nextInt();
+
+                int minimumAge = 7 + (age / 2);
+                String output = age + "-year olds should date somebody who is at least " + minimumAge + " years old.";
+                System.out.println(output);
+
+            }
+        }
+    }}
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Pages {
 
-    public static void main(String[] args) {
+    public Pages(int age) {
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        Pages book = new Pages (age);
+        int pagesToRead = book.calculatePagesToRead();
+
+        System.out.println(age + "-year olds should read at least " + pagesToRead + " pages before giving up on a book.");
+    }
+
+    private int calculatePagesToRead() {
+        return 0;
     }
 }
